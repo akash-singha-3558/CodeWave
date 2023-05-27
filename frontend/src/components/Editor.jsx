844c4ceeb3241f8c569579138c2ac156c1299de9
akash-singha-3558 <akashsingha@gmail.com>
2023-05-27 16:15:33 +0530

    implemented : line number, code editor theme, auto close bracket, auto close tag

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { useEffect } from 'react';
 import CodeMirror from 'codemirror/lib/codemirror';
- import 'codemirror/lib/codemirror.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
 import 'codemirror/mode/javascript/javascript';
-
-
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
 const Editor = () => {
     useEffect(()=>{
-      async function initCodeMirror(){
-        CodeMirror.fromTextArea(document.querySelector("#realTimeEditor"),{
-            mode:{name:"javascript",json:true},
-            theme:'dracula'
-        });
-      }
       initCodeMirror()
     },[])
+
+    async function initCodeMirror(){
+      CodeMirror.fromTextArea(document.querySelector("#realTimeEditor"),{
+          mode:{name:"javascript",json:true},
+          theme:'dracula',
+          autoCloseTags:true,
+          autoCloseBrackets:true,
+          lineNumbers:true,
+          extraKeys: {
+            'Ctrl-Space': 'autocomplete', // Enable auto-suggestion on Ctrl+Space
+          }
+
+      });
+    }
+
   return (
     <textarea id="realTimeEditor">
 
